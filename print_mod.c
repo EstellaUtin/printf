3b9d8d7dcd0067d89b4a604285e6ae132392583b
Ufokwealth <ufokeffiom@gmail.com>
2023-04-28 18:40:33 -0700

    correction on print_mode.c code

--- print_mod.c
+++ print_mod.c
@@ -2,17 +2,24 @@
 #include <unistd.h>
 
 /**
- * print_mod - print %
- *
- * @args: arguments
+ * print_percent - print %
+ * @types: list of arguements
+ * @buffer: buffer array to handle print
+ * @flags: calculate active flags
+ * @width: width
+ * @precision: precision specifiers
+ * @size: size
  * Return: count of chars.
  */
 
-int print_mod(va_list args)
+int print_percent(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
 {
-	(void)args;
-
-	_putchar('%');
-
-	return (1);
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
 }
