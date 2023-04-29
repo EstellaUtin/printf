0a76d53808e20e82bf325029774e909939b13f20
Ufokwealth <ufokeffiom@gmail.com>
2023-04-28 17:28:49 -0700

    code correction for print_string

--- print_string.c
+++ print_string.c
@@ -1,26 +1,46 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 
 /**
- * print_string - print a string
- * @args: user arguements
+ * print_string - function to print a string
+ * @types: list of arguements
+ * @buffer: array to handle print
+ * @flags: calculate active flags
+ * @width: width
+ * @precision: precision specification
+ * @size: size
  * Return: number of characters
  */
-int print_string(va_list args)
+int print_string(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
 {
-	int i;
-	int count_fun = 0;
-	char *str = va_arg(args, char *);
-
-	if (!str)
-		str = "(null)";
-	if (str[0] == '\0')
-		return (-1);
-
-	for (i = 0; str[i] != '\0'; i++)
-		count_fun += _putchar(str[i]);
-
-	return (count_fun);
+	int length = 0, i;
+char *str = va_arg(types, char *);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+if (str == NULL)
+{
+	str = "(null)";
+	if (precision >= 6)
+		str = "
+}
+while (str[length] != '\0')
+length++;
+if (precision >= 0 && precision < length)
+	length = precision;
+	if (width > length)
+{
+	if (flags & F_MINUS)
+{
+	write(1, &str[0], length);
+	for (i = width - length; i > 0; i--)
+		write(1, " ", 1);
+	return (width);
+}
+}
+return (write(1, str, length);
 }
